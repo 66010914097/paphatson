abec385b39f4bf6f1a1f2d63d599607b3d58acbe
66010914097 <66010914097@msu.ac.th>
2026-02-10 12:16:20 +0700

    a

--- i/a.php
+++ i/a.php
@@ -8,7 +8,16 @@
 <body>
 <h1>งาน i</h1>
 <h1>ปภัสสร อุณวงค์ (BB)</h1>
+ <?php
+    include_once("connectdb.php");
+    $sql = "SELECT * FROM `regions` ORDER BY r_name ASC";
+    $rs = mysqli_query($conn,$sql);
 
+    while($data = myaqli_fetch_array($rs)){
+        echo $data['r_id']."". $data['r_name']. "<br>";
+    }
+
+?>
 
 </body>
 </html>
